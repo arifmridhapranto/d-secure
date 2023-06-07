0ccbbe7cd660dd3769f09c09b8b2913b8baf2240
arifmridhapranto <mridhapranto647@gmail.com>
2023-06-07 23:02:07 +0600

    blog page complete

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ $("#v-tabs-tab a").click(function (e) {
   e.preventDefault();
   $(this).tab("show");
 });
+
+$("#font-change").on("input", function () {
+  $(".blog-sub-title ").css("font-size", $(this).val() + "px");
+  console.log($(this).val() + "px");
+  $(".result").html( $(this).val() );
+});
+
+
+$(document).on("input", "#font-change", function () {
+  $("#result").html($(this).val() + "px");
+  $(".blog-sub-title ").css("font-size", $(this).val() + "px");
+});
